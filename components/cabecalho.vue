75cee559018a5d0254a44dc35bc74e8d109ab5b4
LuizFrare <luizeduardo.frare@outlook.com>
2021-11-14 13:00:36 -0300

    Bonitinho

--- components/cabecalho.vue
+++ components/cabecalho.vue
@@ -2,7 +2,7 @@
 <div>
   <header>
 		<div class="logo">
-            <NuxtLink to="/" class="logoImg"><nuxt-img style="height: 100%; width: 100%; max-width: 71px; max-height: 71px; margin-top:5px;" alt="imagem da logo" src="img/LogoWhite.png"/></NuxtLink>
+            <nuxt-Link to="/" class="logoImg"><nuxt-img style="height: 100%; width: 100%; max-width: 71px; max-height: 71px; margin-top:5px;" alt="imagem da logo" src="img/LogoWhite.png"/></nuxt-Link>
         </div>
       <b-navbar toggleable="lg" type="light">
       <b-container class="py-2">
@@ -10,13 +10,12 @@
         <b-collapse class="justify-content-end" id="nav-collapse" is-nav>
           <b-navbar-nav>
             <nuxt-link
-              v-for="item of items"
-              :key="item.url"
-              :to="item.url"
+              v-for="links of items"
+              :key="links.url"
+              :to="links.url"
               class="p-2 link"
-              exact-active-class="underline"
             >
-              {{ item.text }}
+              {{ links.text }}
       </nuxt-link>
           </b-navbar-nav>
         </b-collapse>
@@ -77,16 +76,6 @@ color:#70ecce;
 transition: all .5s ease;
 }
 
-#menu-nav {
-  display: flex;
-  list-style: none;
-  gap: 2rem;
-}
-
-#btn-mobile {
-  display: none;
-}
-
 .logo{
   display: flex;
   width: auto;
@@ -125,10 +114,6 @@ transition: all .5s ease;
 
 @media (max-width: 880px)
 {
-html{
-  font-size: 90%;
-}
-
 .item{
   font-size: 1.1em;
 }
@@ -137,41 +122,8 @@ html{
     display: none;
 }
 
-.img {
-background-image: url("~/static/img/cachorro-inicio.jpg");
-background-size: cover;
-object-fit: cover;
-background-repeat: no-repeat;
-height: 10vh;
-position: relative;
-display: flex;
-min-height: 500px;
-width: auto;
-}
-
-#menu-nav {
-  display: block;
-  position: absolute;
-  width: 100%;
-  top: 70px;
-  right: 0px;
-  background: #0089c9;
-  transition: 0s;
-  z-index: 1000;
-  height: 0px;
-  visibility: hidden;
-}
-
 .buttonHeader{
     margin-right: 50px;
-}
-
-.containerLetters{
-  display: block;
-  width: auto;
-  margin:0 5em;
-  padding-top:5em;
-  text-align: center;
 }
 
 svg{
@@ -183,99 +135,6 @@ svg{
   margin: 0 auto;
   text-align: center;
   padding: 0;
-}
-
-.element{
-  margin:5em 5em;
-}
-
-.containerLetters{
-  display: block;
-  width: auto;
-  margin:0 5em;
-  padding-top:5em;
-  text-align: center;
-}
-  .titulo{
-    margin: 5em;
-  }
-  .category{
-    display: block;
-  }
-  .line{
-    display: none;
-  }
-  .totalLine{
-    margin-top: 3em;
-    display: block;
-  }
-  .category-1{
-    display: flex;
-    flex-direction: row-reverse;
-    font-size: 1.5em;
-  }
-  .containerInformation{
-    text-align: start;
-  }
-  
-.imgInformation{
-    align-self: center;
-    max-width: 100px;
-    max-height: 100px;
-    min-width: 100px;
-    min-height: 100px;
-    margin-bottom: 5em;
-  }
-
-  .card{
-    width: 100%;
-    min-width: 30em;
-    height: 15em;
-  }
-
-  .textContent{
-    font-size: 12px;
-  }
-
-  .imagemInf{
-    margin-top: 1.8em;
-  }
-
-  .imagemInfDog{
-    margin-top: 1.5em;
-  }
-
-  .marginPc{
-    margin-top: 1.8em;
-  }
-
-  .buttonInformation{
-    margin-top:1.5em;
-  }
-}
-
-@media (max-width: 1350px)
-{
-html{
-    font-size: 70%;
-}
-}
-
-@media (max-width: 1100px)
-{
-html{
-    font-size: 80%;
-}
-
-.buttonHeader{
-    margin-right: 1.5em;
-}
-.textContent{
-  font-size: 10px;
-}
-
-.font-size{
-  font-size: calc(10px + 0.5vw);
 }
 }
 
